cd1b0581b5edf931e02f31c760d3865d8876f140
Luke Bayes <lbayes@patternpark.com>
2009-05-23 12:13:27 -0700

    Working through application controller / openid issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,18 @@ class ApplicationController < ActionController::Base
   include ExceptionNotifiable
   include AuthenticatedSystem
   include RoleRequirementSystem
-
+  include OpenIdAuthentication
+  
   helper :all # include all helpers, all the time
   protect_from_forgery :secret => 'b0a876313f3f9195e9bd01473bc5cd06'
   filter_parameter_logging :password, :password_confirmation
   rescue_from ActiveRecord::RecordNotFound, :with => :record_not_found
   
   protected
+  
+  def using_open_id?
+    !params[:openid_url].nil? && params[:openid_url] != ''
+  end
   
   # Automatically respond with 404 for ActiveRecord::RecordNotFound
   def record_not_found
