34f29dabe1e58c6051501e5ade9cbc29bcd4669a
Luke Bayes <lbayes@patternpark.com>
2009-05-23 21:04:57 -0700

    Added warning to commented block of broken code

--- vendor/plugins/facebooker/lib/facebooker/rails/extensions/rack_setup.rb
+++ vendor/plugins/facebooker/lib/facebooker/rails/extensions/rack_setup.rb
@@ -1,4 +1,6 @@
 # Somewhere in 2.3 RewindableInput was removed- rack supports it natively
+puts "[WARNING] Commented out Facebooker / Rack integration, it was failing with Rails 2.3.2"
+
 # require 'rack/facebook'
 # ActionController::Dispatcher.middleware.insert_after( 
 #   (Object.const_get('ActionController::RewindableInput') rescue false) ?
