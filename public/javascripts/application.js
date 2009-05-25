c9996fe13dc5c059b83cfc05f436d3d17d3d64c4
Luke Bayes <lbayes@patternpark.com>
2009-05-24 23:22:32 -0700

    Cleaned up sign in form

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,3 +2,15 @@
 function authenticateWithOpenId(url) {
     $('#openid_url').val(url);
 };
+
+function clearOpenId() {
+    $('#openid_url').val('');
+}
+
+function toggleOpenIdEntry() {
+    var target = $('#any-open-id');
+    var vis = target.is(':visible');
+    target[ vis ? 'hide' : 'show' ]('medium');
+    $('#toggle-any-open-id').text( (vis ? '(+) Show' : '(-) Hide') + ' OpenID URL entry' ); 
+    return false;
+}
